3a5073c5eaa0dc5d9cbd18a3fc0cb3c94891d1be
LAPTOP-TC685HR4\AGILE <bayuagil04@gmail.com>
2025-08-12 15:12:53 +0700

    delete unused file

--- react-day-03-pt02/src/utils/storage.js
+++
@@ -1,8 +0,0 @@
-export function loadScore() {
-  const saved = localStorage.getItem('tic-tac-toe-score');
-  return saved ? JSON.parse(saved) : { win: 0, lose: 0, draw: 0 };
-}
-
-export function saveScore(score) {
-  localStorage.setItem('tic-tac-toe-score', JSON.stringify(score));
-}
